e8c1ed699f25760c0ae0b4bfce45efc6f60204d1
Shepard1808 <florian.bartz.28072003@gmail.com>
2023-07-06 16:19:59 +0200

    forgor the client

---
+++ client/manager.js
@@ -0,0 +1,38 @@
+const socket = new WebSocket('ws://127.0.0.1:8080');
+socket.binaryType = "blob";
+
+console.log(mId);
+
+const onError = (error) => console.error(error);
+const onClose = () => console.log("Disconnected from Server");
+
+
+
+
+socket.onopen = function () {
+    console.log("Connected to Server");
+    console.log(socket.readyState);
+    socket.send(JSON.stringify({
+        from: mId,
+        to: "server",
+        type: "introduction",
+        payload: {},
+        timestamp: new Date()
+    }));
+};
+socket.onerror = onError;
+socket.onclose = onClose;
+
+let response;
+
+//integrate button to send refresh request
+socket.onmessage = function (message){
+    let msg;
+    try{
+        console.log("received:" + message.data);
+        msg = JSON.parse(message.data);
+        console.log(msg.type);
+    }catch (e){
+        console.log("Error: \n" + e);
+    }
+}
